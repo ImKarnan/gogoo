20180b1678de70a8149a90e1dde9bb1bb56c22c9
ImKarnan <109649877+ImKarnan@users.noreply.github.com>
2022-07-21 20:05:50 +0530

    Update blacklist_stickers.py

--- Goku/modules/blacklist_stickers.py
+++ Goku/modules/blacklist_stickers.py
@@ -1,17 +1,17 @@
 import html
 from typing import Optional
 
-import FallenRobot.modules.sql.blsticker_sql as sql
-from FallenRobot import LOGGER, dispatcher
-from FallenRobot.modules.connection import connected
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.alternate import send_message
-from FallenRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from FallenRobot.modules.helper_funcs.misc import split_message
-from FallenRobot.modules.helper_funcs.string_handling import extract_time
+import Goku.modules.sql.blsticker_sql as sql
+from Goku import LOGGER, dispatcher
+from Goku.modules.connection import connected
+from Goku.modules.disable import DisableAbleCommandHandler
+from Goku.modules.helper_funcs.alternate import send_message
+from Goku.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from Goku.modules.helper_funcs.misc import split_message
+from Goku.modules.helper_funcs.string_handling import extract_time
 
-from FallenRobot.modules.log_channel import loggable
-from FallenRobot.modules.warns import warn
+from Goku.modules.log_channel import loggable
+from Goku.modules.warns import warn
 from telegram import Chat, Message, ParseMode, Update, User, ChatPermissions
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, MessageHandler
